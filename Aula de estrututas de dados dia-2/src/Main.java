f9bf853e3597d08c2bc90710ade8586063cf9653
Pedro da Mata Santos <pescariaurbana@gmail.com>
2026-02-05 20:08:07 -0300

    Atividade de fixação

---
+++ Aula de estrututas de dados dia-2/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String [] args){
+
+        byte b = 127;
+
+        int i = 10;
+
+        short s = 300;
+
+        long l = 10000L;
+
+        float f = 50.20F;
+
+        double d = 50.20;
+
+        char c ='A';
+
+        boolean bool = true;
+
+
+        System.out.print(b + "\n" + i +
+                "\n" + s+
+                "\n" + l +
+                "\n" + f+
+                "\n" + d+
+                "\n" + c+
+                "\n" + bool  );
+
+    }
+}
