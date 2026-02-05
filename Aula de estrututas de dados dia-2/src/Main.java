9920447955eaa439d654d40809da498446cda557
Pedro da Mata Santos <pescariaurbana@gmail.com>
2026-02-05 20:12:49 -0300

    Clean code
    Updated variable values and added comments for clarity.

--- Aula de estrututas de dados dia-2/src/Main.java
+++ Aula de estrututas de dados dia-2/src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String [] args){
 
-        byte b = 127;
-
-        int i = 10;
-
-        short s = 300;
-
+        //Inteiros
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
         long l = 10000L;
 
+        //Ponto flutuantes
         float f = 50.20F;
-
         double d = 50.20;
 
+        //Caracter
         char c ='A';
 
+        //Booleano
         boolean bool = true;
 
 
@@ -28,3 +28,4 @@ public class Main {
 
     }
 }
+
